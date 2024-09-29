f8b06d4476953421e26f28df3c90fb9118ac17f4
Neto Lourenço <otaviojnetoo@gmail.com>
2024-09-29 20:38:37 -0300

    ajuste ao rodar o projeto

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  host: true, // Use `true` para permitir acesso na rede local (substitui '0.0.0.0')
+  port: 3000,
 });
