87a91e398a6b1985485f1c115764cf810dd77ceb
Neto Lourenço <otaviojnetoo@gmail.com>
2025-07-12 19:13:28 -0300

    feat: add text and img services

--- src/components/AboutBox/AboutBox.tsx
+++ src/components/AboutBox/AboutBox.tsx
@@ -2,12 +2,31 @@ import React from "react";
 
 const AboutBox: React.FC = () => {
   return (
-    <div className="flex items-center gap-6">
-      <h1>Text</h1>
-      <img
-        src="https://www.rivaincorporadora.com.br/wp-content/uploads/2022/05/Fique-por-dentro-de-como-funciona-o-processo-de-entrega-das-chaves-de-um-apartamento.jpeg.webp"
-        alt=""
-      />
+    <div className="flex flex-col items-center gap-6">
+      <div className="flex items-center gap-8">
+        <h1 className="text-xl font-semibold max-w-[600px] text-balance">
+          Comprar ou vender um imóvel é um processo que envolve diversos
+          trâmites legais e documentação que precisam ser analisados com
+          atenção.
+        </h1>
+        <img
+          className="md:w-[540px] h-[300px] rounded-md"
+          src="/src/assets/analise_imovel.png"
+          alt=""
+        />
+      </div>
+      <div className="flex flex-row-reverse items-center gap-8">
+        <h1 className="text-xl text-balance font-semibold max-w-[600px]">
+          Nosso serviço de consultoria para análise e acompanhamento da compra e
+          venda do imóvel garante que toda a transação ocorra de forma segura,
+          transparente e sem surpresas desagradáveis.
+        </h1>
+        <img
+          className="md:w-[540px] h-[300px] rounded-md"
+          src="/src/assets/analise_de_doc.png"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -3,7 +3,8 @@ import React from "react";
 
 const Services: React.FC = () => {
   return (
-    <div className="pt-[74px]">
+    <div className="pt-[94px]">
+      <h1 className="text-3xl font-semibold text-center mb-8">Serviços</h1>
       <AboutBox />
     </div>
   );
