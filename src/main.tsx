1713e9182b0ec0245f4cc07ccb72184284566e29
Neto Lourenço <otaviojnetoo@gmail.com>
2026-01-31 17:15:28 -0300

    feat: remove styled-components

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
+import { createTheme } from "@mui/material/styles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import {
-  createTheme,
-  ThemeProvider as MuiThemeProvider,
-} from "@mui/material/styles";
 import App from "./App";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 export const muiTheme = createTheme({
   palette: {
     primary: {
@@ -16,9 +13,9 @@ export const muiTheme = createTheme({
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
 
-      <StrictMode>
-        <QueryClientProvider client={queryClient}>
-          <App />
-        </QueryClientProvider>
-      </StrictMode>
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
 );
